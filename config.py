c8d6283ae139be1b94198ad8d0ee7c952803ba22
Axelbhz <axelbhz@gmail.com>
2026-04-08 09:08:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 # ═══════════════════════════════════════════════════════════════
 
 # ── Telegram ────────────────────────────────────────────────────
-TELEGRAM_BOT_TOKEN = "BOT_TOKEN_ICI"
+TELEGRAM_BOT_TOKEN = "8689450824:AAE9NAsDGI9CdLP6qcOvQ3pM5kMnN_6jb-Y"
 
 ALERT_DESTINATIONS = [
-    "CHAT_ID_ICI",
-    "CANAL_ID_ICI",
+    "406477026",
+    "-1003521219534",
 ]
 
 # ── Compétitions surveillées ────────────────────────────────────
